ce9e4bcc205a40d2dc74a87e1c5ff72cf6780e5f
waizkamboh <waizkamboh6@gmail.com>
2025-02-21 16:18:57 +0500

    Variable and data types

--- lecture2/lecture2.cpp
+++ lecture2/lecture2.cpp
@@ -2,6 +2,18 @@
 using namespace std;
 
 int main(){
+    int age = 22;
+    cout<<"Your age is:"<<age<<endl;
+    char grade = 'A';
+    cout<<"Your grade is:"<<grade<<endl;
+    float PI = 3.14;
+    cout<<"Value of PI is:"<<PI<<endl;
+    bool isLogin = false;
+    cout<<"IsLogin:"<<isLogin<<endl;
+    double price = 3.44;
+    cout<<"Price:"<<price<<endl;
     cout<< "Waiz\numer"<<endl;
     return 0;
 }
+
+//Biolerplate Code 
