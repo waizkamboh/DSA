043e10d12aef0474e19e482fd01a4e678027c1f7
waizkamboh <waizkamboh6@gmail.com>
2025-02-21 21:15:32 +0500

    Type casting example and division arhtmetic operator

--- lecture2/lecture2.cpp
+++ lecture2/lecture2.cpp
@@ -26,6 +26,25 @@ int main(){
     cout<<"Enter Number";
     cin>>enterNumber;
     cout<<"You enter number is:"<<enterNumber<<endl;
+    //Arithmetic Operator
+    int a = 10, b = 5;
+    cout<<"Sum of a & b is:"<<a+b<<endl;
+    cout<<"Difference of a & b is:"<<a-b<<endl;
+    cout<<"Product of a & b is:"<<a*b<<endl;
+    cout<<"Division of a & b is:"<<a/b<<endl;
+    cout<<"Modules of a & b is"<<a%b<<endl;
+    //Agar answer decimal me le kar anna ha 
+    int c = 10;
+    float d = 5.10;
+    cout<<c/d<<endl;
+    //Second method
+    cout<<5/(double)2<<endl;
+    //Is dafa answer int me aye ga divide honye ka bad value int ko assign ho rahi ha int data type choti ha wo decimal point ko ignore kar de i
+    int ans = (5 / (double)2);
+    cout<<ans<<endl;
+
+
+
 
     return 0;
 }
@@ -36,3 +55,5 @@ int main(){
 //Conversion(Implicit): Small -> Big
 //casting(Explicit): Big -> Small
 //Conversion implicit hoti ha complier kar ke deta ha  casting Explicit hoti ha programmer kuhd karta ha 
+//Agar hum int ko int se divide karye ge to answer bi int aye ga decimal ke bad wala hissa ignore ho jaye ga agar hm chahtye ha ke answer decimal me aye to ya to hmye float ko int par divide karna parye ga ya int ko float par ya double ko int par ya int ko double par in me se answer os data type me aye ga jo sb se bari ho gi na ke ya division me balke addition, multiplication me bi ya hi rolle ha
+
