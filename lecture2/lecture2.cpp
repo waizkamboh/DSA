a9e7b1c6d06121909f3938968fd0be939a6c4417
waizkamboh <waizkamboh6@gmail.com>
2025-02-21 19:27:50 +0500

    type casting conversion implicit

--- lecture2/lecture2.cpp
+++ lecture2/lecture2.cpp
@@ -2,18 +2,27 @@
 using namespace std;
 
 int main(){
-    int age = 22;
+    int age = 22; //4 Bytes
     cout<<"Your age is:"<<age<<endl;
-    char grade = 'A';
+    char grade = 'A'; //1 Bytes
     cout<<"Your grade is:"<<grade<<endl;
-    float PI = 3.14;
+    float PI = 3.14; //4 Bytes
     cout<<"Value of PI is:"<<PI<<endl;
-    bool isLogin = false;
+    bool isLogin = false; //1 Bytes
     cout<<"IsLogin:"<<isLogin<<endl;
-    double price = 3.44;
+    double price = 3.44; //8 Bytes
     cout<<"Price:"<<price<<endl;
     cout<< "Waiz\numer"<<endl;
+    //Type Casting: Conversion(Implicit)
+    char grade1 = 'A';
+    int value = grade;
+    cout<<value<<endl;
+    
     return 0;
 }
 
 //Biolerplate Code 
+//Type casting : Converting data from one type to another
+//There are two types of types casting
+//Conversion(Implicit): Small -> Big
+//casting(Explicit): Big -> Small
