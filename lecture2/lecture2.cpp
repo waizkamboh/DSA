3fe4db73d576392581d9234e48b11373c3dd1b19
waizkamboh <waizkamboh6@gmail.com>
2025-02-21 20:51:52 +0500

    Casting Explicit

--- lecture2/lecture2.cpp
+++ lecture2/lecture2.cpp
@@ -13,10 +13,16 @@ int main(){
     double price = 3.44; //8 Bytes
     cout<<"Price:"<<price<<endl;
     cout<< "Waiz\numer"<<endl;
-    //Type Casting: Conversion(Implicit)
+    //Type Casting: 
+    //Conversion(Implicit)
     char grade1 = 'A';
     int value = grade;
     cout<<value<<endl;
+    //Casting(Explicit)
+    double price1 = 100.99;
+    int newPrice = (int)price1;
+    cout<<newPrice<<endl;
+
     
     return 0;
 }
@@ -26,3 +32,4 @@ int main(){
 //There are two types of types casting
 //Conversion(Implicit): Small -> Big
 //casting(Explicit): Big -> Small
+//Conversion implicit hoti ha complier kar ke deta ha  casting Explicit hoti ha programmer kuhd karta ha 
