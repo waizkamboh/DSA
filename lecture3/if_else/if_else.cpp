b4fc96aae8a2bc2c3dd74dba16c83562b01c4696
waizkamboh <waizkamboh6@gmail.com>
2025-02-22 21:55:16 +0500

    Check number is positive or negative

---
+++ lecture3/if_else/if_else.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+//Positive or negative number
+int main(){
+    int n;
+    cout<<"Enter a number";
+    cin>>n;
+    if(n >=0){
+        cout<<n<<" Number is positive";
+    }else{
+        cout<<n<<" Number is negative";
+    }
+    return 0;
+}
