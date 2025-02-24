61ecf39305fdac30f9bb4f5b570353ece56d3331
waizkamboh <waizkamboh6@gmail.com>
2025-02-24 23:21:33 +0500

    Sum number from 1 to n using while  loop

--- lecture3/loops/loops.cpp
+++ lecture3/loops/loops.cpp
@@ -16,15 +16,30 @@ using namespace std;
 // }
 
 //Print Number from 1 to N using for loop
+// int main(){
+//     int n;
+//     cout<<"Enter a number";
+//     cin>>n;
+//     for (int i =1; i <= n; i++)
+//     {
+//         cout<<i<<endl;
+//     }
+    
+    
+//     return 0;
+// }
+
+//Sum of number from 1 to N using while loop
+
 int main(){
-    int n;
+    int n, sum = 0, i = 1;
     cout<<"Enter a number";
     cin>>n;
-    for (int i =1; i <= n; i++)
+    while (i <= n)
     {
-        cout<<i<<endl;
+        sum = sum + i;
+        i++;
     }
+    cout<<sum;
     
-    
-    return 0;
 }
