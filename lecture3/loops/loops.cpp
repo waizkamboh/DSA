0abe6678406fc65aa3b92dcb389da9a743a10f61
waizkamboh <waizkamboh6@gmail.com>
2025-02-24 23:16:59 +0500

    Print number from 1 to n using for loop

--- lecture3/loops/loops.cpp
+++ lecture3/loops/loops.cpp
@@ -1,16 +1,30 @@
 #include <iostream>
 using namespace std;
 //Print Number from 1 to N using while loop
+// int main(){
+//     int n;
+//     cout<<"Enter a number";
+//     cin>>n;
+//     int i = 1;
+//     while (i <= n)
+//     {
+//        cout<<i<<endl;
+//        i++;
+//     }
+    
+//     return 0;
+// }
+
+//Print Number from 1 to N using for loop
 int main(){
     int n;
     cout<<"Enter a number";
     cin>>n;
-    int i = 1;
-    while (i <= n)
+    for (int i =1; i <= n; i++)
     {
-       cout<<i<<endl;
-       i++;
+        cout<<i<<endl;
     }
+    
     
     return 0;
 }
