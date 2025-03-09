e99ae1a8daa571f97ba80582592d8b73cd67d8ae
waizkamboh <waizkamboh6@gmail.com>
2025-03-09 22:24:11 +0500

    square_pattern_with_number_sequence

---
+++ lecture4/pattern/square_pattern_with_number_sequence.cpp
@@ -0,0 +1,17 @@
+//Print Square pattern with number
+//1 2 3
+//4 5 6
+//7 8 9
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n = 2, x = 1;
+    for(int i = 0; i <= n; i++){
+        for(int j = 0; j <= n; j++){
+            cout<<x<<" ";
+            x++;
+        }
+        cout<<endl;
+    }
+}
