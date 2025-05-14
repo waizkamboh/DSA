a252a58028b4f715a9ef6ddf58d1cc0bfcfbc22e
waizkamboh <waizkamboh6@gmail.com>
2025-05-14 12:01:09 +0500

    Sum of two number

---
+++ lecture2/sum_of_two_number.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+   int a,b,sum = 0;
+   cout<<"Enter first number";
+   cin>>a;
+   cout<<"Enter second number";
+   cin>>b;
+   sum = a + b;
+   cout<<"Sum of two umber is:"<<sum;
+
+   return 0;
+
+}
