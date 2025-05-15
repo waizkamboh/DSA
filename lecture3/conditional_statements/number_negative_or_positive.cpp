43f05c2201d3ce432cae76d3ca51eae51ee01f93
waizkamboh <waizkamboh6@gmail.com>
2025-05-15 11:38:11 +0500

    conditional statement number is positive or negative

---
+++ lecture3/conditional_statements/number_negative_or_positive.cpp
@@ -0,0 +1,15 @@
+ #include<iostream>
+ using namespace std;
+
+ int main() {
+    int n;
+    cout<<"Enter a number";
+    cin>>n;
+    if(n >= 0){
+        cout<<n<<"Number is positive";
+    }else{
+        cout<<n<<"Number is negative";
+    }
+
+    return 0;
+ }
