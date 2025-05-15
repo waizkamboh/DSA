bbdb6b507b8a43753d0c50cc04736196b36c988e
waizkamboh <waizkamboh6@gmail.com>
2025-05-15 12:24:21 +0500

    ternary statement

---
+++ lecture3/ternary_statement/negative_or_positive.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout<<"Enter a number";
+    cin>>n;
+    cout<< ( n >= 0? "Number is positive" : "Number is negative");
+    return 0;
+}
