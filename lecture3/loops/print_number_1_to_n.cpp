f6f03df6372f28ce01df29b0578c69959f6486da
waizkamboh <waizkamboh6@gmail.com>
2025-05-16 07:46:25 +0500

    print number using user input

---
+++ lecture3/loops/print_number_1_to_n.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n, count = 1;
+    cout<<"Enter a number";
+    cin>>n;
+    while (count <= n)
+    {
+        cout << count << endl;
+        count++;
+    }
+    cout << endl;
+    
+}
