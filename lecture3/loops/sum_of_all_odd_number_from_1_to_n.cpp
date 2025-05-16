4351bad33c04a3553562d1372a115be41064d1ce
waizkamboh <waizkamboh6@gmail.com>
2025-05-16 08:05:52 +0500

    Sum of all odd number form 1 to n using loop

---
+++ lecture3/loops/sum_of_all_odd_number_from_1_to_n.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n, sum = 0;
+    cout <<"Enter a number";
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        if(i % 2 != 0){
+           sum = sum + i;   
+        }
+    }
+    cout <<"Sum of all odd number is:" << sum;
+    
+}
