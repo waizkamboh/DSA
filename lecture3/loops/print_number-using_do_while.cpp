c02329eea547b18fbaef768f59f2a3f9da30703e
waizkamboh <waizkamboh6@gmail.com>
2025-05-16 15:36:01 +0500

    print number using do while loop

---
+++ lecture3/loops/print_number-using_do_while.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n, i = 1;
+    cout << "Enter a number";
+    cin >> n;
+    do
+    {
+        cout << i << endl;
+        i++;
+    } while (i <= n);
+    
+}
