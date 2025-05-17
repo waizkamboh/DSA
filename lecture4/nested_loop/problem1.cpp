f534b76f2c11c64ff4ec14816f8bd414a9e630b0
waizkamboh <waizkamboh6@gmail.com>
2025-05-17 16:35:44 +0500

    nested loop problem1

---
+++ lecture4/nested_loop/problem1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+/*
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+*/
+int main() {
+    int n = 4;
+    for(int i = 0; i <= n; i++){
+        int m = 4;
+        for(int j = 0; j <= m; j++){
+            cout << '*' << ' ';
+        }
+        cout << endl;
+    }
+}
