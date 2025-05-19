41808d662edcea7c8a74c54e11513128db211771
waizkamboh <waizkamboh6@gmail.com>
2025-05-19 12:36:33 +0500

    square pattern with alphabet

--- lecture4/nested_loop/square_pattern_with_alphabet.cpp
+++ lecture4/nested_loop/square_pattern_with_alphabet.cpp
@@ -4,12 +4,14 @@ using namespace std;
 int main() {
     int n = 4;
     for (int i = 0; i < n; i++)
-    {
+    {   
+        char ch = 'A';
         for (int j = 0; j < n; j++)
         {
-            cout << 'A';
+            cout << ch << ' ';
+            ch = ch + 1;
         }
-        
+        cout << endl;
     }
     
     return 0;
