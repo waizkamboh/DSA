4f8ad69ec43e06fb88047380c70f2286867ecb90
waizkamboh <waizkamboh6@gmail.com>
2025-05-19 12:19:16 +0500

    square pattern with number fix infinte loop problem

--- lecture4/nested_loop/square_pattern_with_number.cpp
+++ lecture4/nested_loop/square_pattern_with_number.cpp
@@ -5,12 +5,13 @@ int main() {
     int n = 4;
     for (int i = 0; i < n; i++)
     {
-       for (int j = 0; i < n; j++)
+       for (int j = 0; j < n; j++)
        {
-        cout << j + 1;
+        cout << j + 1 << " ";
        }
+       cout << endl;
+
        
     }
-    
     return 0;
 }
