191831b901028c3a331cb1053c8c0b2d42e85716
waizkamboh <waizkamboh6@gmail.com>
2025-05-21 15:45:49 +0500

    reverse triangle pattern

--- lecture4/nested_loop/reverse_triangle_pattern.cpp
+++ lecture4/nested_loop/reverse_triangle_pattern.cpp
@@ -3,21 +3,23 @@ using namespace std;
 
 /*
 
-1 
-1 2
-1 2 3
-1 2 3 4
+1
+2 1
+3 2 1
+4 3 2 1
 
 */
+
 int main() {
     int n = 4;
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j <= i; j++)
+        for (int j = i; j >= 0; j--)
         {
-            cout << j + 1 << ' ';
+           cout << j + 1 << ' ';
         }
         cout << endl;
     }
+    
     return 0;
 }
