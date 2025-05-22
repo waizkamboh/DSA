bf2766418a81176f5ef6d3503f846a0c551ef15f
waizkamboh <waizkamboh6@gmail.com>
2025-05-22 17:47:00 +0500

    floyd triangle pattern

--- lecture4/nested_loop/floyd_triangle_pattern.cpp
+++ lecture4/nested_loop/floyd_triangle_pattern.cpp
@@ -4,22 +4,23 @@ using namespace std;
 /*
 
 1
-2 1
-3 2 1
-4 3 2 1
+2 3
+4 5 6
+7 8 9 10
 
 */
 
 int main() {
-    int n = 4;
+    int n = 4, num = 1;
     for (int i = 0; i < n; i++)
     {
-        for (int j = i; j >= 0; j--)
+        for (int j = 0; j <= i; j++)
         {
-           cout << j + 1 << '  ';
+            cout << num << " ";
+            num++;
         }
         cout << endl;
     }
-    
     return 0;
+    
 }
