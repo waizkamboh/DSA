a9b210ff768d32e7c4626b07c4d08fa540a15cbc
waizkamboh <waizkamboh6@gmail.com>
2025-05-23 11:32:43 +0500

    function declration and calling with void

---
+++ lecture5/function/function_declration_and_calling_with_void.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+//Function declration or defination
+void printHello() {
+    cout << "Hello World";
+    cout << endl;
+}
+
+int main() {
+
+    //Function invoke or call
+    printHello();
+    printHello();
+    printHello();
+    printHello();
+    printHello();
+    return 0;
+}
