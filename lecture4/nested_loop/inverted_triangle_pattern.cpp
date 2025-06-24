0a5ee4b13444dcc2913893c8dae1ed753a579bc6
waizkamboh <waizkamboh6@gmail.com>
2025-06-24 10:24:10 +0500

    inverted triangle pattern

--- lecture4/nested_loop/inverted_triangle_pattern.cpp
+++ lecture4/nested_loop/inverted_triangle_pattern.cpp
@@ -1,6 +1,26 @@
 #include<iostream>
 using namespace std;
 
+/*
+1 1 1 1
+  2 2 2
+    3 3
+      4
+*/
 int main() {
+    int n = 3;
+   for (int i = 0; i <= n; i++)
+   {
+     for (int j = 0; j < i; j++)
+     {
+       cout << "  ";
+     }
+     for (int j = 0; j <= n - i; j++)
+     {
+        cout << i + 1 << " ";
+     }
+      cout << endl;
+     
+   }
    
 }
