7d8fee43693d4023bad459206892b9820f4f91f4
waizkamboh <waizkamboh6@gmail.com>
2025-07-19 09:30:32 +0500

    bitwise and operator

---
+++ lecture7/bitwise_operator.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a = 4, b = 8;
+    //Bitwise Operator
+    cout << (a & b) << endl;
+    //Bitwise OR
+    cout << (a | b) << endl;
+    return 0;
+}
