72d090133a88b1bbcfa6671825720d25cbe96b98
waizkamboh <waizkamboh6@gmail.com>
2025-07-22 14:57:43 +0500

    accesing the array value

--- lecture8/array.cpp
+++ lecture8/array.cpp
@@ -4,7 +4,15 @@ using namespace std;
 int main() {
     //creating fixed sized array and assign value
     int marks[5] = {100,33,44,22,11};
-
+    cout << marks[0] << endl;
+    cout << marks[1] << endl;
+    cout << marks[2] << endl;
+    cout << marks[3] << endl;
+    cout << marks[4] << endl;
    //creating array and assign value
    double price[] = {11.2,33.4,55.4,6.6};
+   cout << price[0] << endl;
+   cout << price[1] << endl;
+   cout << price[2] << endl;
+   cout << price[3] << endl;
 }
