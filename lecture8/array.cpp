ccd07c875cf4a51632c32c4bf6b970ee4dba024d
waizkamboh <waizkamboh6@gmail.com>
2025-07-22 14:50:24 +0500

    creating and initailizing array

---
+++ lecture8/array.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    //creating fixed sized array and assign value
+    int marks[5] = {100,33,44,22,11};
+
+   //creating array and assign value
+   double price[] = {11.2,33.4,55.4,6.6};
+}
