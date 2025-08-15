7a37be16744aae99cab5873b91e6d1ad5c4cb977
waizkamboh <waizkamboh6@gmail.com>
2025-08-15 11:21:24 +0500

    smallest value of an array using +infinity method

--- lecture8/smallest_value_array.cpp
+++ lecture8/smallest_value_array.cpp
@@ -1,7 +1,9 @@
 #include<iostream>
+#include <climits> // For INT_MAX
 using namespace std;
 
 int main() {
+    //method 1
     int size = 6;
     int num[size] = {5, 15, 22, 1, -15, 24};
    //We assume first index is minmum
@@ -15,5 +17,22 @@ int main() {
     }
 
     cout << "Smallest Value of an array is:" << min;
+
+    //method 2
+    int siz = 6;
+    int nums[siz] = {5, 15, 22, 1, -15, 24};
     
+    //we assume maxnumber is +infinity and compare index of array 
+    int minimum = INT_MAX;
+    for (int i = 0; i < siz; i++)
+    {
+        if (num[i] < minimum)
+        {
+           minimum = num[i];
+        }
+        
+    }
+    cout << "Smallest Value of an array is:" << minimum;
+
+
 }
