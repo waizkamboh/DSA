40b92cb8e034a75639e854d6a1dc089274403a65
waizkamboh <waizkamboh6@gmail.com>
2025-08-15 11:37:34 +0500

    find maximum value using max function

--- lecture8/largest_value_array.cpp
+++ lecture8/largest_value_array.cpp
@@ -5,17 +5,19 @@ int main() {
     int size = 6;
     int num[size] = {5, 15, 22, 1, -15, 24};
     //We assume max number is maximum number
-    int max = num[0];
+    int largest = num[0];
     for (int i = 0; i < size; i++)
     {
-        if (num[i] > max)
-        {
-            max = num[i];
-        }
+        // if (num[i] > largest)
+        // {
+        //     largest = num[i];
+        // }
+        // if we not write if statement we can write max function and find max value using max function
         
+        largest = max(num[i], largest);
     }
 
-    cout << "largest Value of an array is:" << max << endl;
+    cout << "largest Value of an array is:" << largest << endl;
 
     
 }
