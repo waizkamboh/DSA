15f1318c536743b9207e09ef90c8574dc5c651e8
waizkamboh <waizkamboh6@gmail.com>
2025-08-19 12:25:53 +0500

    reverse_even_array_using_2_pointer_approch

---
+++ lecture8/reverse_even_array.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+// Reverse array using 2 pointer approch
+void reverseArray(int arr[], int size) {
+    int start = 0, end = size - 1;
+    while (start < end) {
+      swap(arr[start], arr[end]);
+      start ++;
+      end --;
+    }
+    
+}
+
+int main() {
+    int arr[] = {4, 2, 7, 8, 1, 2};
+    int size = 6;
+    reverseArray(arr, size);
+    for (int i = 0; i < size; i++)
+    {
+       cout << arr[i] <<" ";
+    }
+    
+    
+    return 0;
+
+}
+
