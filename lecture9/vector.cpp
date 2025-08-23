f1d1d205798eb887f65048c76010e3f475b20d34
waizkamboh <waizkamboh6@gmail.com>
2025-08-23 16:36:34 +0500

    printing vector value

--- lecture9/vector.cpp
+++ lecture9/vector.cpp
@@ -4,10 +4,16 @@ using namespace std;
 
 int main() {
     //vector syntax1
-    vector<int> vec;
+    vector<int> vec1;
     //vector syntax2
-    vector<int> vec = {1,2,3};
+    vector<int> vec2 = {1,2,3};
     //vector syntax3
-    vector<int> vec(3, 0);
+    vector<int> vec3(3, 0);
+    cout << vec2[0] << endl;
+    cout << vec2[1] << endl;
+    cout << vec2[2] << endl;
+    cout << vec3[0] << endl;
+    cout << vec3[1] << endl;
+    cout << vec3[2] << endl;
     return 0;
 }
