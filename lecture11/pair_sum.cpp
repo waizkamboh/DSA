2952f1284f8ee72b698992e58bf0a48bd991acd3
waizkamboh <waizkamboh6@gmail.com>
2025-09-03 15:05:59 +0500

    pair sum using brute force approch

---
+++ lecture11/pair_sum.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> pairSum(vector<int> nums, int target) {
+    int size = nums.size();
+    vector<int> ans;
+    for (int i = 0; i < size; i++)
+    {
+       for (int j = i + 1; j < size; j++)
+       {
+         if (nums[i] + nums[j] == target)
+         {
+           ans.push_back(i);
+           ans.push_back(j);
+           return ans;
+         }
+         
+       }
+       
+    }
+    return ans;
+}
+
+int main() {
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 26;
+    vector<int> ans = pairSum(nums, target);
+    cout << ans[0] << " ," << ans[1] << endl;
+    return 0;
+}
