282f9b2e990fa393db24c729e9571e132ebd3ab5
waizkamboh <waizkamboh6@gmail.com>
2025-09-03 15:43:39 +0500

    pair sum using 2 pointer approch

--- lecture11/pair_sum.cpp
+++ lecture11/pair_sum.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
+//This is Brute force approch. Time complexity of this question is o(n^2). This is not better approch to solve this question.
 vector<int> pairSum(vector<int> nums, int target) {
     int size = nums.size();
     vector<int> ans;
@@ -22,10 +22,38 @@ vector<int> pairSum(vector<int> nums, int target) {
     return ans;
 }
 
+//This is 2 pointer approch. This is better approch to solve this question. 
+vector<int> pairSumP(vector<int> nums, int target) {
+    vector<int> ans;
+    int n = nums.size();
+    int i = 0, j = n - 1;
+    while (i < j)
+    {
+        int pairSum = nums[i] + nums[j];
+        if (pairSum > target) {
+            j--;
+        }
+           
+        else if (pairSum < target){
+            i++;
+        }
+        else{
+            ans.push_back(i);
+            ans.push_back(j);
+            return ans; 
+        }
+           
+        
+    }
+    return ans;
+}
+
 int main() {
     vector<int> nums = {2, 7, 11, 15};
     int target = 26;
     vector<int> ans = pairSum(nums, target);
     cout << ans[0] << " ," << ans[1] << endl;
+    vector<int> ansp = pairSumP(nums, target);
+    cout << "Pair sum using 2 pointer approch" << ans[0] <<" ," <<ans[1] << endl;
     return 0;
 }
