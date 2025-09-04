b67ff128094c0af5e032dce8b879ade72620a2f0
waizkamboh <waizkamboh6@gmail.com>
2025-09-04 16:01:41 +0500

    majority element using brute force approch

---
+++ lecture11/majority_element.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int majorityElement(vector<int> nums) {
+     
+    for (int i = 0; i < nums.size(); i++)
+    {
+        int freq = 0;
+        for (int j = 0; j < nums.size(); j++)
+        {
+           if (nums[i] == nums[j])
+           {
+             freq++;
+           }
+           
+        }
+        if (freq = nums.size() / 2)
+        {
+            return nums[i];
+        }
+        
+        
+    }
+   return -1;
+    
+}
+
+int main() {
+    vector<int> nums = {0, 0, 1, 1, 2, 2, 2, 2, 2};
+    cout << majorityElement(nums);
+    return 0;
+}
