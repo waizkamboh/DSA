1bbbd9c6b937d71b00ef711e1968f7dd81d02b2a
waizkamboh <waizkamboh6@gmail.com>
2025-10-14 21:27:30 +0500

    pointer to pointer

--- lecture16/lecture16.cpp
+++ lecture16/lecture16.cpp
@@ -4,11 +4,15 @@ using namespace std;
 int main() {
     int a = 10;
     int* ptr = &a;
+    int** parentPtr = &ptr;
     float price = 100.25;
     float* ptr2 = &price;
+    float** parentPtr2 = &ptr2;
     cout << ptr << endl;
     cout << &a << endl;
     cout << ptr2 << endl;
     cout << &price << endl;
+    cout << parentPtr << endl;
+    cout << parentPtr2 << endl;
     return 0;
 }
