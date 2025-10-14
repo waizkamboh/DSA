02cf984e54c7ae62c14d8132359138c77e7a6091
waizkamboh <waizkamboh6@gmail.com>
2025-10-14 21:09:04 +0500

    Float type pointer

--- lecture16/lecture16.cpp
+++ lecture16/lecture16.cpp
@@ -4,7 +4,11 @@ using namespace std;
 int main() {
     int a = 10;
     int* ptr = &a;
+    float price = 100.25;
+    float* ptr2 = &price;
     cout << ptr << endl;
     cout << &a << endl;
+    cout << ptr2 << endl;
+    cout << &price << endl;
     return 0;
 }
