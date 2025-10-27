a9c4b1a50874f8ea9470ec6ac1a3b38869723782
waizkamboh <waizkamboh6@gmail.com>
2025-10-27 10:31:06 +0500

    add array element at end

--- lecture8/add_element.cpp
+++ lecture8/add_element.cpp
@@ -23,21 +23,34 @@ using namespace std;
 
 // Add array element at the specific position
 
+// int main() {
+//     int arr[10] = {1, 3, 5, 7, 9},  n = 5, pos = 1;
+
+//     for (int i = n; i > pos; i--)
+//     {
+//         arr[i] = arr[i -1];
+//     }
+
+//     arr[1] = 6;
+//     n++;
+//     for (int i = 0; i < n; i++)
+//     {
+//         cout << arr[i];
+//     }
+//     return 0;
+    
+    
+// }
+
+// Add array element at the end 
 int main() {
-    int arr[10] = {1, 3, 5, 7, 9},  n = 5, pos = 1;
-
-    for (int i = n; i > pos; i--)
-    {
-        arr[i] = arr[i -1];
-    }
-
-    arr[1] = 6;
+    int arr[10] = {1, 3, 5, 7, 9} ,  n = 5;
+    arr[5] = 6;
     n++;
     for (int i = 0; i < n; i++)
     {
         cout << arr[i];
     }
-    return 0;
     
-    
+
 }
