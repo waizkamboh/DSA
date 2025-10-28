3c645e96e23090e2232d65aed99ea6b5f5e0b7a1
waizkamboh <waizkamboh6@gmail.com>
2025-10-28 17:04:30 +0500

    Remove specfic element form array

--- lecture8/remove_element.cpp
+++ lecture8/remove_element.cpp
@@ -1,18 +1,52 @@
 #include <iostream>
 using namespace std;
 
+// Remove first element from array
+// int main() {
+//     int arr[] = {1, 6, 3, 5, 3, 7};
+//     int n = 6;
+//     for (int i = 0; i < n; i++)
+//     {
+//         arr[i] = arr[i + 1];
+//     }
+//     n--;
+//     for (int i = 0; i < n; i++)
+//     {
+//         cout << arr[i];
+//     }
+    
+//     return 0;
+// }
+
+ // Remove specfic element from array
+// int main() {
+    
+//     int arr[] = {1, 6, 3, 5, 3, 7};
+//     int n = 6, pos = 1;
+//     for (int i = pos; i < n; i++)
+//     {
+//         arr[i] = arr[i + 1];
+//     }
+//     n--;
+//     for (int i = 0; i < n; i++)
+//     {
+//         cout << arr[i];
+//     }
+    
+//     return 0;
+
+// }
+
+// Remove last element from array
 int main() {
+    
     int arr[] = {1, 6, 3, 5, 3, 7};
     int n = 6;
-    for (int i = 0; i < n; i++)
-    {
-        arr[i] = arr[i + 1];
-    }
     n--;
     for (int i = 0; i < n; i++)
     {
         cout << arr[i];
     }
     
-    return 0;
+
 }
