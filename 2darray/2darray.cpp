7fbeb1589b0fba1b31b9e84706b0432c29d7d033
waizkamboh <waizkamboh6@gmail.com>
2025-10-30 14:45:39 +0500

    declare and initialize array

--- 2darray/2darray.cpp
+++ 2darray/2darray.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-    int matrix[4][3];
+    int matrix[4][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {10, 11, 12}};
     int rows = 4;
     int col = 3;
     return 0;
