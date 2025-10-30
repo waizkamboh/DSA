21d01c1604780802eda33af6abd113709bc9d7ae
waizkamboh <waizkamboh6@gmail.com>
2025-10-30 14:53:59 +0500

    Input element using 2d array

--- 2darray/2darray.cpp
+++ 2darray/2darray.cpp
@@ -5,5 +5,15 @@ int main() {
     int matrix[4][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {10, 11, 12}};
     int rows = 4;
     int col = 3;
+    int table[4][3];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            cin >> table[i][j];
+        }
+        
+    }
+    
     return 0;
 }
