b389f7d2ea85a4f1e0aae9dc2bbcf4d99d21766c
waizkamboh <waizkamboh6@gmail.com>
2025-11-03 09:42:52 +0500

    declare and reinitilize string

--- string/string.cpp
+++ string/string.cpp
@@ -3,11 +3,9 @@
 using namespace std;
 
 int main() {
-    string str = "Waiz";
-    string str1("Umer");
-    cout << str <<" " << str1;
-    string str2;
-    //cin >> str2;
-    getline(cin, str2);
-    cout << str2;
+    string str = "Hello World";
+    cout << str << endl;
+    str = "Hello";
+    cout << str << endl;
+
 }
