cd9b83b11f4a86d60dff2ecb9f1d379b2a32472c
raomudassar34 <112935849+raomudassar34@users.noreply.github.com>
2025-11-09 21:28:58 +0500

    parameterized constructor

--- oop/oop.cpp
+++ oop/oop.cpp
@@ -13,7 +13,14 @@ public:
 
   // Non parameterized constructor
   Teacher() {
-    dep = "Computer Science";
+    cout << "I am non parameterized constructor";
+  }
+
+  Teacher(string n, string d, string s, float sal) {
+    name = n;
+    dep = d;
+    sub = s;
+    salary = sal;
   }
   
   //Methods / Member function
@@ -21,24 +28,18 @@ public:
       dep = newDep;
   }
 
-  void setSalary(int s) {
-    salary = s;
-  }
-
-  double getSalary() {
-    return salary;
+  void getInfo() {
+      cout << "Name : " << name << endl;
+      cout << "Department : " << dep << endl;
+      cout << "Subject : " << sub << endl;
+      cout << "Salary : " << salary << endl;
+      
   }
 };
 
 int main() {
-    Teacher t1;
-    t1.name = "Waiz";
-    t1.sub = "Programming";
-    t1.setSalary(25000);
-    cout << t1.name << endl;
-    cout << t1.dep << endl;
-    cout << t1.sub << endl;
-    cout << t1.getSalary() << endl;
-
+    //Teacher t1("Waiz", "Computer Science", "Programming", 25000);
+    Teacher t1 = Teacher("Waiz", "Computer Science", "Programming", 25000);
+    t1.getInfo();
     return 0;
 }
