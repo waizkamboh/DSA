4223bb7a4e309a2106b3e76fb17d7379157c847f
raomudassar34 <112935849+raomudassar34@users.noreply.github.com>
2025-11-27 11:31:13 +0500

    push fornt in Linked List

--- linkedList/linked_list.cpp
+++ linkedList/linked_list.cpp
@@ -23,16 +23,33 @@ class List {
      }
 
      void push_front(int val) {
-        Node* newNode = new Node(val);
-        if (head == NULL)
-        {
-            head = tail = newNode;
-            return;
-        }
-        
+      Node* newNode = new Node(val);
+      if(head == NULL) {
+        head = tail = newNode;
+        return;
+      }else {
+        newNode->next = head;
+        head = newNode;
+      }
      }
+
+     void print() {
+      Node* temp = head;
+      while (temp != NULL) {
+        cout << temp->data << "->";
+        temp = temp->next;
+      }
+      cout << "NULL";
+      
+     }
+
 };
 
 int main() {
+   List ll;
+   ll.push_front(1);
+   ll.push_front(2);
+   ll.push_front(3);
+   ll.print();
     return 0;
 }
