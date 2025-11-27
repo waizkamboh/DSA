7733ba134b4d83a5fc8ef647365559c52bba7b6f
raomudassar34 <112935849+raomudassar34@users.noreply.github.com>
2025-11-27 11:41:36 +0500

    Push Back in Linked List

--- linkedList/linked_list.cpp
+++ linkedList/linked_list.cpp
@@ -33,6 +33,18 @@ class List {
       }
      }
 
+     void push_back(int val) {
+       Node* newNode = new Node(val);
+       if (head == NULL)
+       {
+        head = tail = newNode;
+       }else {
+        tail->next = newNode;
+        tail = newNode;
+       }
+       
+     }
+
      void print() {
       Node* temp = head;
       while (temp != NULL) {
@@ -50,6 +62,7 @@ int main() {
    ll.push_front(1);
    ll.push_front(2);
    ll.push_front(3);
+   ll.push_back(4);
    ll.print();
     return 0;
 }
