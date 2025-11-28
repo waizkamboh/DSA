70aaaab2371fec0f5caa54b7e2351f02731135e3
raomudassar34 <112935849+raomudassar34@users.noreply.github.com>
2025-11-28 21:26:02 +0500

    print

--- linkedList/linked_list.cpp
+++ linkedList/linked_list.cpp
@@ -58,7 +58,15 @@ class List {
       
      }
 
-     
+     void print() {
+      Node* temp = head;
+      while (temp != NULL) {
+        cout << temp->data << "->";
+        temp = temp->next;
+      }
+      cout << "NULL";
+      
+     }
 
 };
 
