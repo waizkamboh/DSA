8b0e935ec8399b22b5af4c5b46f709638d943c36
raomudassar34 <112935849+raomudassar34@users.noreply.github.com>
2025-12-02 18:25:56 +0500

    Middle of a Linked List

---
+++ linkedList/middle_node.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+  
+    public:
+      int data;
+      Node* next;
+
+      Node(int val) {
+        data = val;
+        next = NULL;
+      }
+};
+
+class List {
+   Node* head;
+   Node* tail;
+
+   public:
+     List() {
+        head = tail = NULL;
+     }
+
+     void push_front(int val) {
+      Node* newNode = new Node(val);
+      if(head == NULL) {
+        head = tail = newNode;
+        return;
+      }else {
+        newNode->next = head;
+        head = newNode;
+      }
+     }
+
+     
+     void print() {
+      Node* temp = head;
+      while (temp != NULL) {
+        cout << temp->data << "->";
+        temp = temp->next;
+      }
+      cout << "NULL";
+      
+     }
+
+     void middleOfLL() {
+      int len = 0;
+      Node* temp = head;
+      Node* temp1 = head;
+      while (temp != NULL)
+      {
+        temp = temp->next;
+        len++;
+      }
+      int middleNode = ((len/2) + 1);
+      for (int i = 1; i < middleNode; i++)
+      {
+        temp1 = temp1->next;
+      }
+
+      cout << "Middle of a LinkedList" << temp1->data;
+      
+      
+     }
+
+
+
+};
+
+int main() {
+   List ll;
+   ll.push_front(5);
+   ll.push_front(4);
+   ll.push_front(3);
+   ll.push_front(2);
+   ll.push_front(1);
+   ll.print();
+   cout << endl;
+   ll.middleOfLL();
+   return 0;
+}
