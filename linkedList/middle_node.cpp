b3df31a94b39c4d943de99aef4ffcadc1e1e1fc2
raomudassar34 <112935849+raomudassar34@users.noreply.github.com>
2025-12-03 22:14:13 +0500

    Fixing bugs in slow fast approch

--- linkedList/middle_node.cpp
+++ linkedList/middle_node.cpp
@@ -70,7 +70,7 @@ class List {
       Node* slow = head;
       Node* fast = head;
 
-      while (fast->next != NULL && fast != NULL)
+      while (fast!= NULL && fast->next != NULL)
       {
         slow = slow->next;
         fast = fast->next->next;
